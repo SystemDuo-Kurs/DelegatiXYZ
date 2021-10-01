e66eaea05aa9a902d6382c4bc695363d5bcfebc3
Dragan Pepic <d.pepic@hotmail.com>
2021-10-01 18:03:22 +0200

    Add project files.

---
+++ DelegatiXYZ/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DelegatiXYZ
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Predavac pred = new();
+			Polaznik p1 = new(pred);
+			Polaznik p2 = new(pred);
+			Diktafon d1 = new(pred);
+			
+			pred.Govori("Nesto tamo");
+	
+		}
+	}
+
+	class Predavac
+	{
+		public event Action<string> GovorDelegat;
+
+		public void Govori(string govor)
+		{
+			//if (GovorDelegat is not null)
+			//	GovorDelegat.Invoke(govor);
+			//Isto kao:
+			GovorDelegat?.Invoke(govor);
+		}
+	}
+
+	class Polaznik
+	{
+		public Polaznik(Predavac p)
+		{
+			p.GovorDelegat += Slusam;
+		}
+		public void Slusam(string nesto)
+		{
+			Console.WriteLine($"Cuo sam {nesto}");
+		}
+	}
+	class Diktafon
+	{
+		public Diktafon(Predavac p)
+		{
+			p.GovorDelegat += Snimaj;
+		}
+		public void Snimaj(string bla) => Console.WriteLine("Diktafon sljaka");
+	}
+
+}
